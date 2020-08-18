87779052fdded011619297e8edd34bf65bfe9b5b
rapsealk <piono623@naver.com>
2020-08-18 18:48:44 +0900

    Add: space button

--- win32py/keyboard_hook.py
+++ win32py/keyboard_hook.py
@@ -16,15 +16,20 @@ WH_KEYBOARD_LL = 13
 WM_KEYDOWN = 0x0100
 
 CTRL_CODE = 29
+F1_CODE = 59
 
 KEY_MAP = {
     16: 'q', 17: 'w', 18: 'e', 19: 'r', 20: 't',
     21: 'y', 22: 'u', 23: 'i', 24: 'o', 25: 'p',
+    29: 'Ctrl',
     30: 'a', 31: 's', 32: 'd', 33: 'f', 34: 'g',
     35: 'h', 36: 'j', 37: 'k', 38: 'l',
     44: 'z', 45: 'x', 46: 'c', 47: 'v', 48: 'b',
-    49: 'n', 50: 'm'
+    49: 'n', 50: 'm',
+    57: 'Space', 59: 'F1'
 }
+
+TARGET_KEYS = ['q', 'e', 'w', 's', 'Space']
 
 
 class KeyboardHook:
@@ -75,7 +80,7 @@ def hook_procedure(nCode, wParam, lParam):
                                                    lParam)
     lparam = (lParam[0] >> 32)  # and 0xFFFF
 
-    if CTRL_CODE == int(lparam):
+    if F1_CODE == int(lparam):
         print("Ctrl pressed, call uninstallHook()")
         key_hook.uninstall_hook_procedure()
         sys.exit(-1)
@@ -87,7 +92,8 @@ def hook_procedure(nCode, wParam, lParam):
                                                    nCode,
                                                    wParam,
                                                    lParam)
-    key_hook.queue.put((int(time.time() * 1000), hooked_key))
+    if hooked_key in TARGET_KEYS:
+        key_hook.queue.put((int(time.time() * 1000), hooked_key))
     return ctypes.windll.user32.CallNextHookEx(key_hook.hooked,
                                                nCode,
                                                wParam,
